bde82fc77b338eb46d7c77a025de5179f1aac74e
SelvinLP <36603066+SelvinLP@users.noreply.github.com>
2020-02-08 23:05:31 -0600

    Creacion del proyecto

---
+++ OLC1_Practica1_201701133/src/olc1_practica1_201701133/OLC1_Practica1_201701133.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olc1_practica1_201701133;
+
+/**
+ *
+ * @author Aragon Perez
+ */
+public class OLC1_Practica1_201701133 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
